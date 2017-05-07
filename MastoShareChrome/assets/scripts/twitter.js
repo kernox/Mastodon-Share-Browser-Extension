16a5651287b1f2b78e6d5953a4901a9ce0849b0c
Kernox <kernox@live.fr>
2017-05-07 18:00:47 +0200

    Retrait zepto pour twitter.js

--- MastoShareChrome/assets/scripts/twitter.js
+++ MastoShareChrome/assets/scripts/twitter.js
@@ -2,70 +2,49 @@ var stream = document.querySelector('#stream-items-id');
 
 stream.addEventListener('click', addMenu);
 
-/*var action = document.querySelector('.ProfileTweet-action');
-action.style.border = '2px solid red';
-//console.log(action);
-
-//action.addEventListener('click', "addMenu");
-*/
-
 function addMenu(event){
 
 	if(event.target.className.match('Icon'))
 	{
-		alert('test');
+		var menu = document.createElement('li');
+		menu.className='share-via-mastodon';
+		menu.dataset.nav = 'share_tweet_dm';
+		menu.setAttribute('role', 'presentation');
+		menu.addEventListener('click', shareViaMastodon);
+
+		var button = document.createElement('button');
+		button.type = 'button';
+		button.className ='dropdown-link';
+		button.setAttribute('role', 'menuitem');
+		button.innerHTML = 'Partager via Mastodon';
+
+		menu.appendChild(button);
+
+		var that = event.target;
+		var menu_action = that.parentNode.parentNode.nextElementSibling.querySelector('ul');
+
+		var firstElementOfMenu = menu_action.firstElementChild;
+
+		if(firstElementOfMenu.className!=menu.className)
+			menu_action.insertBefore(menu, firstElementOfMenu);
 	}
-	
 
 	return false;
 }
 
-/*function addMenu()
-{
-	this.style.border = '2px solid red';
-	var existMenu = document.querySelector('.share-via-mastodon');
+function shareViaMastodon(event){
 
-	if(existMenu)
-		existMenu.remove();
+	var tweetDIV = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+	var author_fullname = tweetDIV.dataset.name;
+	var author_username = tweetDIV.dataset.screenName;
+	var permalink = 'https://twitter.com'+tweetDIV.dataset.permalinkPath;
+	var tweet = tweetDIV.querySelector('.tweet-text').innerText;
 
-	var menu = document.createElement('li');
-	menu.className='share-via-mastodon';
-	menu.dataset.nav = 'share_tweet_dm';
-	menu.setAttribute('role', 'presentation');
+	var message = author_fullname + "  " + author_username + "\n" + tweet + "\n\n" + permalink;
 
-	var button = document.createElement('button');
-	button.type = 'button';
-	button.className ='dropdown-link';
-	button.setAttribute('role', 'menuitem');
-	button.innerHTML = 'Partager via Mastodon';
+	chrome.storage.sync.get(null, function(items){
+		instanceUrl = items.instanceUrl;
+		sendToMastodonFromTwitter(instanceUrl, message);
+	});
 
-	var target = document.querySelector('li.share-via-dm');
-	//target.parentNode.insertBefore(menu, target);
-	console.log(target.parentNode);
-	//console.log(target);
-}*/
-
-/*$('#stream-items-id').delegate('.ProfileTweet-action', 'click', function(){
-	$('.share-via-mastodon').remove();
-
-	$('li.share-via-dm').before(
-		'<li class="share-via-mastodon" data-nav="share_tweet_dm" role="presentation">'+
-			'<button type="button" class="dropdown-link" role="menuitem">Partager via Mastodon</button>'+
-			'</li>'
-	);
-
-	$('.share-via-mastodon button').on('click', function(){
-		var content = $(this).parents('.tweet');
-		var author_fullname = content.data('name');
-		var author_username = '@'+content.data('screen-name');
-		var permalink = 'https://twitter.com'+content.data('permalink-path');
-		var tweet = content.find('.tweet-text').text();
-
-
-		chrome.storage.sync.get(null, function(items){
-			instanceUrl = items.instanceUrl;
-			var message = author_fullname + "  " + author_username + "\n" + tweet + "\n\n" + permalink;
-			sendToMastodonFromTwitter(instanceUrl, message);
-		});
-	})
-});*/
+}
