6d4a9750f275c01cff0b8550ebe7078f5e0dbf30
Kernox <kernox@live.fr>
2017-05-08 12:38:07 +0200

    Correction innerHTML

--- MastoShareFirefox/assets/scripts/popup.js
+++ MastoShareFirefox/assets/scripts/popup.js
@@ -7,7 +7,7 @@ chrome.storage.sync.get(null, function(items){
 	shortner = items.shortner;
 	loading_message = items.loading_message;
 
-	document.body.innerHTML = loading_message;
+	document.body.innerText = loading_message;
 
 	//If mastodon instance not configured
 	if (instanceUrl == '' || instanceUrl == undefined || instanceUrl == 'https://')
