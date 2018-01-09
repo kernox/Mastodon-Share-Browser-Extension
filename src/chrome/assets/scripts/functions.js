4a317286c9486a1597c08105c34b892db95d4143
Alexis Gilard <alexis.gilard@gmail.com>
2018-01-09 01:55:41 +0100

    Add some translations

--- src/chrome/assets/scripts/functions.js
+++ src/chrome/assets/scripts/functions.js
@@ -10,18 +10,23 @@ function loadLocale(code){
 
 	getJSON('assets/locales/' + code + '.json', function(lang){
 		document.querySelector('#startInfo').innerText= lang.start_info;
+		document.querySelector('#btnConnectInstance').value = lang.obtain_access_code;
 		document.querySelector('#status').innerText=lang.options_saved;
 		document.querySelector('#options .panel-heading').innerText=lang.settings;
+		document.querySelector('label[for="code"]').innerText=lang.access_code;
+		document.querySelector('label[for="accesskey"]').innerText=lang.access_key;
 		document.querySelector('label[for="instanceUrl"]').innerText=lang.instance_url;
 		document.querySelector('label[for="shortner"]').innerText=lang.short_url_checkbox;
 		document.querySelector('label[for="language"]').innerText=lang.language;
 		document.querySelector('#instanceUrlHelp').innerText=lang.url_form_needed;
 		document.querySelector('#save').value = lang.save;
 
-		chrome.storage.sync.set({
+
+
+		/*chrome.storage.sync.set({
 			loading_message: lang.mastodon_instance_opening.split(' ').join('\u00a0'),
 			share_selection: lang.share_selection
-		});
+		});*/
 	});
 }
 
