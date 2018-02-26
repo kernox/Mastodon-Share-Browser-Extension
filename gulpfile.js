862c59ea28fe676464c3379b720a118ed25cec73
Alexis Gilard <alexis.gilard@gmail.com>
2018-02-26 15:43:26 +0100

    Unstable gulp script version

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 var watch = require('gulp-watch');
 var preprocess = require('gulp-preprocess');
+var fs = require('fs');
 
 gulp.task('default',[
 	'build',
@@ -9,43 +10,33 @@ gulp.task('default',[
 ]);
 
 gulp.task('watch', function(){
-	watch(['src/all/**/*', '!src/all/**/*.html', '!src/all/**/*.js'], {verbose: true})
-	.pipe(gulp.dest('build/firefox'))
-	.pipe(gulp.dest('build/chrome'))
 
-	watch(['src/all/**/*.html', 'src/all/**/*.js'], {verbose: true})
-	.pipe(preprocess({context: {ENV: 'chrome'}}))
-	.pipe(gulp.dest('build/chrome'))
+	//watch(['src/**/*.html'], {verbose: true})
+	//.pipe(preprocess({context: {ENV: 'chrome'}}))
+	//.pipe(gulp.dest('build/chrome'))
 
-	watch(['src/all/**/*.html', 'src/all/**/*.js'])
-	.pipe(preprocess({context: {ENV: 'firefox'}}))
-	.pipe(gulp.dest('build/firefox'))
-
-	watch('src/firefox/**/*', {verbose: true})
-	.pipe(gulp.dest('build/firefox'))
-
-	watch('src/chrome/**/*', {verbose: true})
-	.pipe(gulp.dest('build/chrome'))
+	//watch(['src/**/*'])
+	//.pipe(preprocess({context: {ENV: 'firefox'}}))
+	//.pipe(gulp.dest('build/firefox'))
 });
 
 gulp.task('build', function(){
-	gulp.src(['src/all/**/*', '!src/all/**/*.html', '!src/all/**/*.js'])
-	.pipe(gulp.dest('build/firefox'))
-	.pipe(gulp.dest('build/chrome'))
 
-	gulp.src(['src/all/**/*.html', 'src/all/**/*.js'])
+	gulp.src(['src/**/*.html', 'src/**/*.js', 'src/**/*.css', 'src/**/*.json', '!src/manifest.json'])
 	.pipe(preprocess({context: {ENV: 'chrome'}}))
 	.pipe(gulp.dest('build/chrome'))
 
-	gulp.src(['src/all/**/*.html', 'src/all/**/*.js'])
+	gulp.src(['src/**/*.html', 'src/**/*.js', 'src/**/*.css', 'src/**/*.json', '!src/manifest.json'])
 	.pipe(preprocess({context: {ENV: 'firefox'}}))
 	.pipe(gulp.dest('build/firefox'))
 
-	gulp.src('src/firefox/**/*')
+	gulp.src(['src/**/*.png'])
+	.pipe(gulp.dest('build/chrome'))
 	.pipe(gulp.dest('build/firefox'))
 
-	gulp.src('src/chrome/**/*')
-	.pipe(gulp.dest('build/chrome'))
+	buildManifest('chrome');
+	//buildManifest('firefox');
+
 });
 
 gulp.task('pack', function(){
@@ -58,3 +49,70 @@ gulp.task('pack', function(){
 		.pipe(zip('chrome.zip'))
 		.pipe(gulp.dest('build'))
 });
+
+var buildManifest = function(browser){
+	var json = {};
+
+	json.manifest_version = 2;
+
+	if(browser == 'firefox'){
+		json.applications = {
+			gecko: {
+				id: "{09b14d46-21c3-4a7d-b244-e756f497935b}",
+				strict_min_version: "52.0"
+			}
+		}
+	}
+
+	json.name = "Mastodon Share";
+	json.description = "A share button for Mastodon Instances";
+	json.version = "0.4"
+
+	if(browser == 'firefox') {
+		json.options_ui = {
+			page: "options.html"
+		}
+	}
+
+	json.browser_action = {
+		default_icon : "assets/images/icon.png",
+		default_popup: "popup.html"
+	}
+
+	json.content_scripts = [
+		{
+			matches : ["https://twitter.com/*"],
+      		js: [
+        		"assets/scripts/functions.js",
+        		"assets/scripts/twitter.js"
+      		]
+		}
+	];
+
+	json.background = {
+    	scripts: [
+      "assets/scripts/interceptor.js",
+      "assets/scripts/menu.js",
+      "assets/scripts/functions.js"
+    	]
+  	};
+
+  	json.permissions = [
+		"contextMenus",
+		"cookies",
+		"<all_urls>",
+		"tabs",
+		"activeTab",
+		"storage"
+	];
+
+	json.icons= {
+		"16": "assets/images/icon16.png",
+		"32": "assets/images/icon32.png",
+		"64": "assets/images/icon64.png"
+	};
+
+	var dump = JSON.stringify(json);
+
+	fs.writeFileSync('build/'+ browser +'/manifest.json', dump);
+}
