8eaa823b17db37a9597368feeed224676196847a
Peter Ludikovsky <peter.ludikovsky@gmail.com>
2018-08-27 15:07:33 +0200

    Improve i18n script

--- tools/i18n.sh
+++ tools/i18n.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 lang=en
+chrome=$( which chrome )
+
+if [ "x${chrome}" == "x" ]; then
+  chrome=$( which google-chrome )
+fi
+
+if [ "x${chrome}" == "x" ]; then
+  chrome=$( which chromium-browser )
+fi
 
 echo Availables locales for Mastodon Share
 echo
@@ -9,4 +18,7 @@ echo
 
 read -p "Please set a locale code [en]: " lang
 
-chrome --lang=$lang --user-data-dir=/tmp/chrome-profile-$lang --load-extension=$PWD/../build/chrome
+export LANGUAGE=${lang}.UTF-8
+rm -rf /tmp/chrome-profile-${lang}
+
+${chrome} --lang=${lang} --user-data-dir=/tmp/chrome-profile-${lang} --load-extension=${PWD}/../build/chrome
