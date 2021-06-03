7c304d5763ee6329b11b7a9944737c1ead072ada
Alexis Gilard <alexis@e-major.fr>
2021-06-03 13:36:03 +0200

    Transformations vue js

--- src/assets/scripts/options.js
+++ src/assets/scripts/options.js
@@ -1,3 +1,151 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        labels: {
+            instanceUrl: chrome.i18n.getMessage('instance_url'),
+            settings: chrome.i18n.getMessage('settings'),
+            obtainAccessCode: chrome.i18n.getMessage('obtain_access_code'),
+            accessCode: chrome.i18n.getMessage('access_code'),
+            accessKey: chrome.i18n.getMessage('access_key'),
+            shortUrlCheckbox: chrome.i18n.getMessage('short_url_checkbox'),
+            tootMaxSize: chrome.i18n.getMessage('toot_max_size'),
+            defaultDisclaimer: chrome.i18n.getMessage('default_disclaimer'),
+            save: chrome.i18n.getMessage('save'),
+            optionsSaved: chrome.i18n.getMessage('options_saved')
+        },
+        api: null,
+        instanceUrl : "",
+        accessKey: "",
+        accessCode: "",
+        defaultDisclaimer,
+        shortner: false
+    },
+    mounted: function(){
+        this.loadOptions();
+        // console.log(chrome.storage.sync.get({
+        //     instanceUrl: ''
+        // }));
+    },
+    methods: {
+        create: function() {
+            console.log('start');
+        },
+        connectInstance: function(){
+            chrome.storage.sync.set({
+                accessKey: ''
+            }, function(){
+                this.accessKey = '';
+                this.accessCode = 'nop';
+            });   
+        
+            this.api = new MastodonAPI({
+                instance: this.instanceUrl,
+                api_user_token: ""
+            });
+        
+            this.openAuthDialog();
+        },
+        openAuthDialog: function() {
+            var that = this;
+            //We do not have user token, need to register the app
+            this.api.registerApplication("Mastodon Share", 'urn:ietf:wg:oauth:2.0:oob', ['read', 'write'],'', function(data) {
+
+                //Save client id, secret and redirect uri
+                chrome.storage.sync.set({
+                    "mastodon_client_id": data["client_id"],
+                    "mastodon_client_secret": data["client_secret"],
+                    "mastodon_client_redirect_uri": data['redirect_uri']
+                });
+
+                //Generate the authorization link
+                var authorization = that.api.generateAuthLink(data["client_id"],
+                    'urn:ietf:wg:oauth:2.0:oob',
+                    "code",
+                    ["read", "write"]
+                );
+
+                //Open the authentification window
+                chrome.tabs.create({url: authorization});
+            });
+
+            console.log(this.api);
+        },
+        loadOptions: function(){
+            console.log('Loading options...');
+            chrome.storage.sync.get({
+                instanceUrl: '',
+                shortner: false,
+                accessKey: '',
+                code: '',
+                tootMaxSize: 500,
+                defaultDisclaimer: ''
+            }, function(items) {
+                this.instanceUrl = items.instanceUrl;
+                this.shortner = items.shortner;
+                this.accessKey = items.accessKey;
+                this.code = items.code;
+                this.tootMaxSize = items.tootMaxSize;
+                this.defaultDisclaimer = items.defaultDisclaimer;
+            });
+        },
+        saveOptions: function() {
+
+            var that = this;
+
+            var status = document.querySelector('#status');
+
+            chrome.storage.sync.set({
+                instanceUrl: instanceUrl,
+                shortner: this.useShortner,
+                code: this.accessCode,
+                tootMaxSize: this.tootMaxSize,
+                defaultDisclaimer: this.defaultDisclaimer
+            }, function() {
+
+                status.classList.remove('hide');
+                that.status = that.labels.optionsSaved;
+                document.querySelector('#startInfo').classList.add('hide');
+
+                setTimeout(function() {
+                    status.classList.add('hide');
+                }, 2000);
+
+                that.api = new MastodonAPI({
+                    instance: that.instanceUrl,
+                    api_user_token: ""
+                });
+
+                chrome.storage.sync.get(null, function(items){
+
+                    if (items.accessKey === ''){
+
+                        console.log(items);
+
+                        api.getAccessTokenFromAuthCode(
+                            items.mastodon_client_id,
+                            items.mastodon_client_secret,
+                            items.mastodon_client_redirect_uri,
+                            items.code,
+                            function(data) {
+                                chrome.storage.sync.set({
+                                    accessKey: data.access_token
+                                }, function(){
+                                    this.accessKey = data.access_token;
+                                });
+                            }
+                        );
+                    }
+                });
+            });
+        }
+    }
+});
+
+
+
+/*
+
+
 function init() {
 
 	loadMessages();
@@ -5,21 +153,8 @@ function init() {
     //loadInstancesList();
 }
 
-function loadMessages(){
-	document.title = chrome.i18n.getMessage('settings');
-	document.querySelector('label[for="instanceUrl"]').innerText = chrome.i18n.getMessage('instance_url');
-	document.querySelector('#options .panel-heading').innerText = chrome.i18n.getMessage('settings');
-	document.getElementById('btnConnectInstance').value = chrome.i18n.getMessage('obtain_access_code');
-	document.querySelector('label[for="code"]').innerText = chrome.i18n.getMessage('access_code');
-	document.querySelector('label[for="accesskey"]').innerText = chrome.i18n.getMessage('access_key');
-	document.querySelector('label[for="shortner"]').innerText = chrome.i18n.getMessage('short_url_checkbox');
-	document.querySelector('label[for="tootMaxSize"]').innerText = chrome.i18n.getMessage('toot_max_size');
-	document.querySelector('label[for="defaultDisclaimer"]').innerText = chrome.i18n.getMessage('default_disclaimer');
-	document.getElementById('save').value = chrome.i18n.getMessage('save');
-}
-
 function loadInstancesList() {
-	/* find all instances known to the browser */
+	// find all instances known to the browser
 
     //@if ENV='chrome'
     //var browser = chrome;
@@ -42,7 +177,7 @@ function loadInstancesList() {
 
     	if (mastodonInput.value.length)
     		return;
-    	/* none configured, pick the first one with a user session */
+    	// none configured, pick the first one with a user session
     	browser.cookies.getAll({name: "remember_user_token"}).then((cookies) => {
     		for (let cookie of cookies) {
     			let url = (cookie.secure ? "https://" : "http://") + cookie.domain;
@@ -54,118 +189,4 @@ function loadInstancesList() {
     	});
     });
 }
-
-function loadOptions() {
-    chrome.storage.sync.get({
-        instanceUrl: '',
-        shortner: false,
-        accessKey: '',
-        code: '',
-        tootMaxSize: 500,
-        defaultDisclaimer: ''
-    }, function(items) {
-        document.querySelector('#instanceUrl').value = items.instanceUrl;
-        document.querySelector('#shortner').checked = items.shortner;
-        document.querySelector('#accessKey').value = items.accessKey;
-        document.querySelector('#code').value = items.code;
-        document.querySelector('#accessKey').value = items.accessKey;
-        document.querySelector('#tootMaxSize').value = items.tootMaxSize;
-        document.querySelector('#defaultDisclaimer').value = items.defaultDisclaimer;
-    });
-}
-
-function saveOptions(e) {
-    e.preventDefault();
-
-    var status = document.querySelector('#status');
-
-    chrome.storage.sync.set({
-        instanceUrl: document.querySelector('#instanceUrl').value,
-        shortner: document.querySelector('#shortner').checked,
-        code: document.querySelector('#code').value,
-        tootMaxSize: document.querySelector('#tootMaxSize').value,
-        defaultDisclaimer: document.querySelector('#defaultDisclaimer').value
-    }, function() {
-
-        status.classList.remove('hide');
-        status.innerText = chrome.i18n.getMessage('options_saved');
-        document.querySelector('#startInfo').classList.add('hide');
-
-        setTimeout(function() {
-            status.classList.add('hide');
-        }, 2000);
-
-        var api = new MastodonAPI({
-            instance: instanceUrl.value,
-            api_user_token: ""
-        });
-
-
-        chrome.storage.sync.get(null, function(items){
-
-            if (items.accessKey === ''){
-
-                api.getAccessTokenFromAuthCode(
-                    items.mastodon_client_id,
-                    items.mastodon_client_secret,
-                    items.mastodon_client_redirect_uri,
-                    items.code,
-                    function(data) {
-                        chrome.storage.sync.set({
-                            accessKey: data.access_token
-                        }, function(){
-                            document.querySelector('#accessKey').value = data.access_token;
-                        });
-                    }
-                );
-            }
-        });
-    });
-}
-
-function connectInstance(){
-
-    chrome.storage.sync.set({
-        accessKey: ''
-    }, function(){
-        document.querySelector('#accessKey').value='';
-        document.querySelector('#code').value='';
-    });
-
-    var instanceUrl = document.getElementById('instanceUrl').value;
-
-    var api = new MastodonAPI({
-        instance: instanceUrl,
-        api_user_token: ""
-    });
-
-    openAuthDialog(api);
-}
-
-
-function openAuthDialog(api){
-    //We do not have user token, need to register the app
-    api.registerApplication("Mastodon Share", 'urn:ietf:wg:oauth:2.0:oob', ['read', 'write'],'', function(data) {
-
-        //Save client id, secret and redirect uri
-        chrome.storage.sync.set({
-            "mastodon_client_id": data["client_id"],
-            "mastodon_client_secret": data["client_secret"],
-            "mastodon_client_redirect_uri": data['redirect_uri']
-        });
-
-        //Generate the authorization link
-        var authorization = api.generateAuthLink(data["client_id"],
-            'urn:ietf:wg:oauth:2.0:oob',
-            "code",
-            ["read", "write"]
-        );
-
-        //Open the authentification window
-        chrome.tabs.create({url: authorization});
-    });
-}
-
-document.addEventListener('DOMContentLoaded', init);
-document.querySelector('#options').addEventListener('submit', saveOptions);
-document.getElementById('btnConnectInstance').addEventListener('click', connectInstance);
+*/
